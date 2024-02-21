3e785df9e6487fcb7fa206ca9cbebc456e13f76d
hko15HT <157689934+hko15HT@users.noreply.github.com>
2024-02-22 06:53:30 +0900

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,51 @@
+#! py -3.11
+
+import time
+import speech_recognition
+import pyaudio
+
+SAMPLERATE = 44100
+
+def callback(in_data, frame_count, time_info, status):
+    global sprec
+    try:
+        audiodata = speech_recognition.AudioData(in_data,SAMPLERATE,2)
+        sprec_text = sprec.recognize_google(audiodata, language='ja-JP')
+        print(sprec_text)
+
+    except speech_recognition.UnknownValueError as e:
+        print(e)
+    except speech_recognition.RequestError as e:
+        print(e)
+    finally:
+        return (None, pyaudio.paContinue)
+    
+def main():
+    global sprec
+    sprec = speech_recognition.Recognizer()  # インスタンスを生成
+    sprec.energy_threshold = 0
+    # Audio インスタンス取得
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open( format = pyaudio.paInt16,
+                        rate = SAMPLERATE,
+                        channels = 1,
+                        input_device_index = 1,
+                        input = True,
+                        frames_per_buffer = SAMPLERATE*2, # 2秒周期でコールバック
+                        stream_callback=callback)
+    
+    stream.start_stream()
+    while stream.is_active():
+        time.sleep(0.1)
+    
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    
+
+    
+if __name__ == '__main__':
+    main()
+
+
